277b23811b8809e83db27cfe6c6e28442aa13a11
vekaomg <146497829+vekalmao@users.noreply.github.com>
2024-06-20 09:44:04 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/signup',
+        destination: '/app/SignUp', // Adjust the destination path as per your actual path
+      },
+    ];
+  },
+};
